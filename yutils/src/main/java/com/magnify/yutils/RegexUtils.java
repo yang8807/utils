df032a6a9054a1eeb9cb38f64a59e6f43381a83b
heinigger <939301889@qq.com>
2016-08-02 20:57:54 +0800

    添加了隔离双符号的表达式

--- yutils/src/main/java/com/magnify/yutils/RegexUtils.java
+++ yutils/src/main/java/com/magnify/yutils/RegexUtils.java
@@ -1,4 +1,4 @@
-package com.magnify.yutils;
+package utils;
 
 import java.util.regex.Pattern;
 
@@ -6,17 +6,17 @@ import java.util.regex.Pattern;
  * Created by  on 2016/8/2.
  */
 public class RegexUtils {
-    //
+    //手机号码
     public final static String REGEX_PHONE_NUMBER = "^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$";
-    //
+    //电子邮箱
     public final static String REGEX_EMAIL = "[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?";
-    //
+    //网址
     public final static String REGEX_URL = "[a-zA-z]+://[^\\s]*";
-    //
+    //身份证号码
     public final static String REGEX_USER_ID_NUMBER = "^(\\d{6})(\\d{4})(\\d{2})(\\d{2})(\\d{3})([0-9]|X)$";
-    //
+    //中文
     public final static String REGEX_IS_CHINESE = "[\\u4e00-\\u9fa5]";
-    //,
+    //双字符,包括中文
     public final static String REGEX_DBCS = "[^\\x00-\\xff]";
 
     public static boolean isMobileNumber(String mobiles) {
