71e42eb59a5ade9c25698902fd4061741a5e5ecb
huangyanwu <huangyanwu@eelly.net>
2016-08-05 23:35:01 +0800

    delete

---
+++ app/src/main/java/com/magnify/utils/base/CurrentApp.java
@@ -0,0 +1,27 @@
+package com.magnify.utils.base;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.datautils.RandomUserUtil;
+import com.magnify.basea_dapter_library.ViewHolder;
+
+/**
+ * Created by heinigger on 16/8/5.
+ */
+public class CurrentApp extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = super.getApplicationContext();
+
+        RandomUserUtil.init(context);
+        ViewHolder.setImageLoaderInterface(new GlideImageLoader());
+    }
+
+    public static Context getAppContext() {
+        return context;
+    }
+}
