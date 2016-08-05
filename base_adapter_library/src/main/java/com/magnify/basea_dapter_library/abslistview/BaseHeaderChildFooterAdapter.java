94ba33fe3fe32ae7f6393b972dba9cb9b3053cca
huangyanwu <huangyanwu@eelly.net>
2016-08-05 23:37:44 +0800

    修改了类似订单效果的adapter

--- base_adapter_library/src/main/java/com/magnify/basea_dapter_library/abslistview/BaseHeaderChildFooterAdapter.java
+++ base_adapter_library/src/main/java/com/magnify/basea_dapter_library/abslistview/BaseHeaderChildFooterAdapter.java
@@ -16,58 +16,131 @@ import java.util.ArrayList;
  * H:Header_AND_Footer
  * C:Child
  * 类似淘宝的那种订单效果,
- * 头部显示价钱之类的,中间显示商品,数量不定,尾部显示商品的总计数量和价钱总和
+ * 头部显示价钱之类的,中间显示商品,数量不定,尾部显示商品的总计数量和价钱总和;
+ * 1.可以头部,中部,尾部使用$link(BaseHeaderChildFooterAdapter(Context mContext, ArrayList<HF> groupDatas, int headerLayout, int childLayout, int footerLayout))
+ * 2.可以头部,中部使用#link(BaseHeaderChildFooterAdapter(Context mContext, ArrayList<HF> groupDatas, int headerLayout, int childLayout)
+ * 3.中部,尾部使用#link(BaseHeaderChildFooterAdapter(int footerLayout, Context mContext, ArrayList<HF> groupDatas, int childLayout)
  */
 public abstract class BaseHeaderChildFooterAdapter<HF, C> extends BaseAdapter {
 
-    private ArrayList<HF> headerDatas;
+    private ArrayList<HF> groupDatas;
     private Context mContext;
-    private int headerLayout;
+    private int headerLayout = -1;
     private int childLayout;
-    private int footerLayout;
+    private int footerLayout = -1;
     //存放所有header或者footer的位置,0是header,1是存放的就是footer的位置
     private SparseArray<PositionInfo> positions = new SparseArray<>();
     private int positionCounter;
-    /*在此中的三种类型*/
-    private static final int TYPE_HEADER = 23;
-    private static final int TYPE_CHILD = 24;
-    private static final int TYPE_FOOTER = 25;
+    /*在此中的三种类型,只能用0,1,2这种递增方式来进行标志,不然会报错*/
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_CHILD = 1;
+    private static final int TYPE_FOOTER = 2;
     private static final int TYPE_COUNT = 3;
     private LayoutInflater infalter;
+    /*上次groupdata的数量,用来判断是不是需要重新遍历一遍数据*/
+    private int lastDataSize;
+    /*遍历数组开始的位置*/
+    private int traveseIndex = 0;
 
 
-    public BaseHeaderChildFooterAdapter(Context mContext, ArrayList<HF> headerDatas, int headerLayout, int childLayout, int footerLayout) {
-        this.headerDatas = headerDatas;
+    public BaseHeaderChildFooterAdapter(Context mContext, ArrayList<HF> groupDatas, int headerLayout, int childLayout, int footerLayout) {
+        this.groupDatas = groupDatas;
         this.mContext = mContext;
         this.headerLayout = headerLayout;
         this.childLayout = childLayout;
         this.footerLayout = footerLayout;
         infalter = LayoutInflater.from(mContext);
-        if (headerLayout==footerLayout||childLayout==footerLayout||headerLayout==childLayout){
+        if (headerLayout == footerLayout || childLayout == footerLayout || headerLayout == childLayout) {
             throw new IllegalMonitorStateException("you are supported to use diffent");
         }
+        this.lastDataSize = groupDatas == null ? 0 : groupDatas.size();
+        caculateTypePosition();
+
+    }
+
+    public BaseHeaderChildFooterAdapter(Context mContext, ArrayList<HF> groupDatas, int headerLayout, int childLayout) {
+        this.groupDatas = groupDatas;
+        this.mContext = mContext;
+        this.headerLayout = headerLayout;
+        this.childLayout = childLayout;
+        infalter = LayoutInflater.from(mContext);
+        if (headerLayout == childLayout) {
+            throw new IllegalMonitorStateException("you are supported to use diffent");
+        }
+        this.lastDataSize = groupDatas == null ? 0 : groupDatas.size();
+        caculateTypePosition();
+    }
+
+    /**
+     * @param footerLayout
+     * @param mContext
+     * @param groupDatas
+     * @param childLayout
+     */
+    public BaseHeaderChildFooterAdapter(int footerLayout, Context mContext, ArrayList<HF> groupDatas, int childLayout) {
+        this.groupDatas = groupDatas;
+        this.mContext = mContext;
+        this.footerLayout = footerLayout;
+        this.childLayout = childLayout;
+        infalter = LayoutInflater.from(mContext);
+        if (childLayout == footerLayout) {
+            throw new IllegalMonitorStateException("you are supported to use diffent");
+        }
+        this.lastDataSize = groupDatas == null ? 0 : groupDatas.size();
         caculateTypePosition();
     }
 
     /*计算每个位置需要返回什么类型View*/
     private void caculateTypePosition() {
-        positionCounter = 0;
-        if (headerDatas != null && !headerDatas.isEmpty())
-            for (int i = 0; i < headerDatas.size(); i++) {
-                //key 存放在的是类型的position,用完再增加
-                positions.put(positionCounter, new PositionInfo(i, TYPE_HEADER));
-                //得到当前组的数量,这里都是存放child的
-                for (int j = 0; j < getChildCount(headerDatas.get(i), i); j++) {
-                    positions.put(++positionCounter, new PositionInfo(i, TYPE_CHILD, j));
-                }//这里才是存放footer的
-                positions.put(++positionCounter, new PositionInfo(i, TYPE_FOOTER));
-                positionCounter++;
+        if (groupDatas != null && !groupDatas.isEmpty()) {
+            for (int i = traveseIndex; i < groupDatas.size(); i++) {
+                if (headerLayout > 0) {//headerlayout使用的时候
+                    //key 存放在的是类型的position,用完再增加
+                    positions.put(positionCounter, new PositionInfo(i, TYPE_HEADER));
+                    //得到当前组的数量,这里都是存放child的
+                    for (int j = 0; j < getChildCount(groupDatas.get(i), i); j++) {
+                        positionCounter += 1;
+                        positions.put(positionCounter, new PositionInfo(i, TYPE_CHILD, j));
+                    }//这里才是存放footer的
+                    positionCounter += 1;
+                    if (footerLayout > 0) {//没有footer部分
+                        positions.put(positionCounter, new PositionInfo(i, TYPE_FOOTER));
+                        positionCounter += 1;
+                    }
+                } else {//headerlayout不使用的时候
+                    for (int j = 0; j < getChildCount(groupDatas.get(i), i); j++) {
+                        positions.put(positionCounter, new PositionInfo(i, TYPE_CHILD, j));
+                        positionCounter += 1;
+                    }//这里才是存放footer的
+                    positions.put(positionCounter, new PositionInfo(i, TYPE_FOOTER));
+                    positionCounter += 1;
+                }
             }
+        }
+    }
+
+    /*强制重新计算所有位置,保证每个数据返回都是正确的,仅再对数据源做了加和减操作的时候,不然效率有问题,其实也不是什么问题...,比起view嵌套之类的...*/
+    public void forceNotifyDataSetChanged() {
+        lastDataSize = 0;
+        traveseIndex = 0;
+        positionCounter = 0;
+        positions.clear();
+        caculateTypePosition();
+        super.notifyDataSetChanged();
     }
 
     @Override
     public void notifyDataSetChanged() {
         //当通知数据改变的时候,需要重新记录数据呀,如果是下拉刷新的化,可能第一个item的child数量变了,导致所有的位置都变了,那么就需要重新计算位置
+        if (groupDatas == null) return;
+        if (lastDataSize >= groupDatas.size())//数据比上次的还少
+        {
+            traveseIndex = 0;
+            positionCounter = 0;
+            positions.clear();
+        } else {//存在的问题,如果使用者先将集合数据删掉一些,再添加一些,那么开始的位置就会出现问题了...,那么可以使用强制刷新,重新将位置计算一遍
+            traveseIndex = lastDataSize;
+        }
         caculateTypePosition();
         super.notifyDataSetChanged();
     }
@@ -85,7 +158,7 @@ public abstract class BaseHeaderChildFooterAdapter<HF, C> extends BaseAdapter {
     @Override
     public int getCount() {
         //头部加尾部+孩子的数量
-        return (headerDatas == null ? 0 : headerDatas.size()) * 2 + getAllChildCount();
+        return positions.size();
     }
 
     @Override
@@ -95,10 +168,10 @@ public abstract class BaseHeaderChildFooterAdapter<HF, C> extends BaseAdapter {
         int type = positionInfo.getType();
 
         if (type == TYPE_HEADER || type == TYPE_FOOTER) {
-            return headerDatas.get(positionInfo.getGroupPosition());
+            return groupDatas.get(positionInfo.getGroupPosition());
         } else {
             //哪个group的哪个child?
-            return getChild(headerDatas.get(positionInfo.getGroupPosition()), positionInfo.getChildPosition());
+            return getChild(groupDatas.get(positionInfo.getGroupPosition()), positionInfo.getChildPosition());
         }
     }
 
@@ -111,13 +184,14 @@ public abstract class BaseHeaderChildFooterAdapter<HF, C> extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        int type = getItemViewType(position);
+        int type = 0;
         ViewHolder headerHolder = null;
         ViewHolder childHolder = null;
         ViewHolder footerHolder = null;
         PositionInfo positionInfo = positions.get(position);
         int groupPosition = positionInfo.getGroupPosition();
         int childPosition = positionInfo.getChildPosition();
+        type = positionInfo.getType();
         if (convertView == null) {//当返回类型的View为空的时候
             switch (type) {
                 case TYPE_HEADER:
@@ -150,7 +224,7 @@ public abstract class BaseHeaderChildFooterAdapter<HF, C> extends BaseAdapter {
             }
         }
 
-        HF hf = headerDatas.get(groupPosition);
+        HF hf = groupDatas.get(groupPosition);
 
         //在这里开始设置各个布局所应该做的事情了
         switch (type) {
@@ -179,9 +253,6 @@ public abstract class BaseHeaderChildFooterAdapter<HF, C> extends BaseAdapter {
 
     /*得到子Item*/
     protected abstract C getChild(HF hf, int childPosition);
-
-    /*得到子类所有的数量*/
-    public abstract int getAllChildCount();
 
     /*得到当前组的子类数量*/
     public abstract int getChildCount(HF hf, int groupPosition);
