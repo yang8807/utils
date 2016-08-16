5684d361c975ae756e89a936cfc6a23b39e4d07d
huangyanwu <huangyanwu@eelly.net>
2016-08-16 10:57:48 +0800

    perfect looper effect:真正完美的实现轮播...(当切换时间和停留时间超过范围,假的无线循环就露出破绽...)

--- app/src/main/java/com/magnify/utils/ui/ui_view/ActivityBannerView.java
+++ app/src/main/java/com/magnify/utils/ui/ui_view/ActivityBannerView.java
@@ -21,31 +21,46 @@ public class ActivityBannerView extends CurrentBaseActivity implements View.OnCl
         setContentView(R.layout.activity_looper_view);
         setOnClickListener(this, R.id.btn_net, R.id.btn_local);
         banner_looper_view = (BannerLoopView) findViewById(R.id.banner_looper_view);
+        setNetWorkAdapter();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_net://用来加载网络图片
-                banner_looper_view.setImageUrls(R.layout.item_banner_looper, new BannerLoopView.LoopAdapterListener<String>() {
-                            @Override
-                            public void convert(ViewHolder viewHolder, int position, String s) {
-                                viewHolder.displayImage(s, R.id.imageView);
-                            }
-                        },
-                        RandomUtil.getRandomImage(4));
+                setNetWorkAdapter();
                 break;
             case R.id.btn_local://用来加载本地图片
-                banner_looper_view.setImageUrls(R.layout.item_banner_looper, new BannerLoopView.LoopAdapterListener<Integer>() {
-                            @Override
-                            public void convert(ViewHolder viewHolder, int position, Integer s) {
-                                viewHolder.displayImage(s, R.id.imageView);
-                            }
-                        },
-                        R.drawable.ic_watch_later_blue_grey_700_36dp,
-                        R.drawable.ic_perm_identity_green_500_36dp,
-                        R.drawable.ic_https_blue_600_36dp);
+                setLocalAdapter();
                 break;
         }
+    }
+
+    /**
+     * 用来加载本地的数据
+     */
+    private void setLocalAdapter() {
+        banner_looper_view.setImageUrls(R.layout.item_banner_looper, new BannerLoopView.LoopAdapterListener<Integer>() {
+                    @Override
+                    public void convert(ViewHolder viewHolder, int position, Integer s) {
+                        viewHolder.displayImage(s, R.id.imageView);
+                    }
+                },
+                R.drawable.ic_watch_later_blue_grey_700_36dp,
+                R.drawable.ic_perm_identity_green_500_36dp,
+                R.drawable.ic_https_blue_600_36dp);
+    }
+
+    /**
+     * 用来加载网络的图片数据
+     */
+    private void setNetWorkAdapter() {
+        banner_looper_view.setImageUrls(R.layout.item_banner_looper, new BannerLoopView.LoopAdapterListener<String>() {
+                    @Override
+                    public void convert(ViewHolder viewHolder, int position, String s) {
+                        viewHolder.displayImage(s, R.id.imageView);
+                    }
+                },
+                RandomUtil.getRandomImage(4));
     }
 }
