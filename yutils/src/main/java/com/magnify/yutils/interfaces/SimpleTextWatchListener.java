95605216ab5e8f8fb44411c9f5729436acf77490
huangyanwu <huangyanwu@eelly.net>
2016-08-22 10:35:51 +0800

    SimpleTextWatcher

---
+++ yutils/src/main/java/com/magnify/yutils/interfaces/SimpleTextWatchListener.java
@@ -0,0 +1,24 @@
+package com.magnify.yutils.interfaces;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by heinigger on 16/8/22.
+ */
+public class SimpleTextWatchListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
