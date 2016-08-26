15c414bc1531b62a0fc8d9c6bdf051b047a09bb9
huangyanwu <huangyanwu@eelly.net>
2016-08-26 21:39:49 +0800

    Animations

--- app/src/main/java/com/magnify/utils/ui/ui_animation/ViewAnimationActivity.java
+++ app/src/main/java/com/magnify/utils/ui/ui_animation/ViewAnimationActivity.java
@@ -1,19 +1,21 @@
 package com.magnify.utils.ui.ui_animation;
 
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.magnify.utils.R;
 import com.magnify.utils.base.CurrentBaseActivity;
 import com.magnify.yutils.LogUtils;
-import com.magnify.yutils.ToastUtil;
 
 /**
  * Created by heinigger on 16/8/26.
  */
 public class ViewAnimationActivity extends CurrentBaseActivity implements View.OnClickListener {
     private View viewAnimation1, viewAnimation2, viewAnimation3, viewAnimtion3, viewAnimation4;
+    private ViewGroup mViewGoup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,10 @@ public class ViewAnimationActivity extends CurrentBaseActivity implements View.O
         viewAnimation2 = findViewById(R.id.view_anim2);
         viewAnimation3 = findViewById(R.id.view_anim3);
         viewAnimation4 = findViewById(R.id.view_anim4);
+
+        mViewGoup = (ViewGroup) findViewById(R.id.lly_parent);
+
+
         viewAnimation1.setOnClickListener(this);
         viewAnimation2.setOnClickListener(this);
         viewAnimation3.setOnClickListener(this);
@@ -43,7 +49,14 @@ public class ViewAnimationActivity extends CurrentBaseActivity implements View.O
                 getTransLationAnimation(viewAnimation3, viewAnimation4).start();
                 break;
             case R.id.view_anim4:
-                getTransLationAnimation(viewAnimation1, viewAnimation2).start();
+                for (int i = 0; i < mViewGoup.getChildCount(); i++) {
+                    if (i + 1 < mViewGoup.getChildCount()) {
+                        ObjectAnimator objectAnimator = getTransLationAnimation(mViewGoup.getChildAt(i), mViewGoup.getChildAt(i + 1));
+                        objectAnimator.setStartDelay(i * 100);
+                        objectAnimator.start();
+                    }
+                }
+
                 break;
         }
     }
@@ -52,10 +65,9 @@ public class ViewAnimationActivity extends CurrentBaseActivity implements View.O
         if (endView.getMeasuredHeight() == 0) {
             endView.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.AT_MOST), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.EXACTLY));
         }
-        LogUtils.v("mine", endView.getMeasuredHeight());
+        LogUtils.v("mine", startView.getBottom() + ":" + endView.getMeasuredHeight());
         endView.setVisibility(View.VISIBLE);
         //动画是以启动动画的这个View为起点,相对位置
-        ToastUtil.show(self, startView.getBottom() + ":" + endView.getMeasuredHeight());
-        return ObjectAnimator.ofFloat(endView, "translationY", -startView.getMeasuredHeight(), 0).setDuration(1000);
+        return ObjectAnimator.ofPropertyValuesHolder(endView, PropertyValuesHolder.ofFloat("translationY", -startView.getMeasuredHeight(), 0), PropertyValuesHolder.ofFloat("alpha", 0, 1)).setDuration(500);
     }
 }
