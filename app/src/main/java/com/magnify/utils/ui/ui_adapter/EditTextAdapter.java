ddc3ae35261d643dd673874125e0498e1ca72ec7
huangyanwu <huangyanwu@eelly.net>
2016-08-28 01:10:27 +0800

    ......

--- app/src/main/java/com/magnify/utils/ui/ui_adapter/EditTextAdapter.java
+++ app/src/main/java/com/magnify/utils/ui/ui_adapter/EditTextAdapter.java
@@ -1,6 +1,9 @@
 package com.magnify.utils.ui.ui_adapter;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.util.SparseArray;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +13,7 @@ import com.magnify.basea_dapter_library.ViewHolder;
 import com.magnify.basea_dapter_library.abslistview.CommonAdapter;
 import com.magnify.utils.R;
 import com.magnify.yutils.LogUtils;
+import com.magnify.yutils.interfaces.SimpleTextWatchListener;
 
 import java.util.List;
 
@@ -17,6 +21,9 @@ import java.util.List;
  * Created by heinigger on 16/8/27.
  */
 public class EditTextAdapter extends CommonAdapter<String> {
+
+    private SparseArray<String> backString = new SparseArray<>();
+
     public EditTextAdapter(Context context, List datas) {
         super(context, R.layout.item_edit_text, datas);
     }
@@ -25,7 +32,8 @@ public class EditTextAdapter extends CommonAdapter<String> {
     @Override
     public void convert(ViewHolder holder, final int position, String o) {
         LogUtils.v("mine", position + ":" + o);
-        holder.setText(R.id.edit_text, o).<EditText>getView(R.id.edit_text);
+        mDatas.set(position, TextUtils.isEmpty(backString.get(position)) ? o : backString.get(position));
+        holder.setText(R.id.edit_text, TextUtils.isEmpty(backString.get(position)) ? o : backString.get(position)).<EditText>getView(R.id.edit_text);
         holder.<EditText>getView(R.id.edit_text).setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
@@ -34,13 +42,40 @@ public class EditTextAdapter extends CommonAdapter<String> {
             }
         });
 
+
         final EditText mEditext = holder.<EditText>getView(R.id.edit_text);
         mEditext.setTag(position);
+        mEditext.setHint(o);
+        //还存在一个问题,只有当按删除键的时候,才会触发
+        mEditext.addTextChangedListener(new SimpleTextWatchListener() {
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (TextUtils.isEmpty(editable.toString())) {
+                    if (TextUtils.isEmpty(backString.get(position))) {
+                        mDatas.set(position, editable.toString());
+                    }
+                } else {
+                    mDatas.set(position, editable.toString());
+                }
+            }
+        });
         mEditext.setOnKeyListener(new EditText.OnKeyListener() {
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEv) {
+                LogUtils.v("mine", "获取characters", keyEv.getCharacters());
+                switch (keyEv.getAction()) {
+                    case KeyEvent.ACTION_DOWN:
+                        LogUtils.v("mine", "按下键盘");
+                        break;
+                    case KeyEvent.ACTION_UP:
+                        LogUtils.v("mine", "松开键盘");
+                        break;
+                }
                 int position = (int) view.getTag();
-                mDatas.set(position,((EditText)view).getText().toString());
+                String text = ((EditText) view).getText().toString();
+                mDatas.set(position, text);
+                backString.put(position, text);
                 LogUtils.v("mine", "onKeyEvent,我什么时候触发", ((EditText) view).getText().toString());
                 return false;
             }
