fb796969b0257cd51b07ca9966526fbb5beedf15
huangyanwu <huangyanwu@eelly.net>
2016-08-28 01:09:40 +0800

    无比兴奋,终于解决了ListView中嵌套EditText的问题了

--- yutils/src/main/java/com/magnify/yutils/interfaces/SimpleTextWatchListener.java
+++ yutils/src/main/java/com/magnify/yutils/interfaces/SimpleTextWatchListener.java
@@ -2,15 +2,11 @@ package com.magnify.yutils.interfaces;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.widget.EditText;
 
 /**
  * Created by heinigger on 16/8/22.
  */
-public class SimpleTextWatchListener implements TextWatcher {
-    private EditText editText;
-    public SimpleTextWatchListener(EditText editText) {
-    }
+public abstract class SimpleTextWatchListener implements TextWatcher {
 
     @Override
     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -19,7 +15,6 @@ public class SimpleTextWatchListener implements TextWatcher {
 
     @Override
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
     }
 
     @Override
